3ddb9c9ebf79d16af3a39b45cd9ce1f6d232343f
Pokuri Karthik <Karthikpokuri13@gmail.com>
2026-02-23 12:37:46 +0530

    Initial commit - pagination app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Pagination from "./Component/Pagination";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Book Pagination</h1>
+      <Pagination />
+    </div>
+  );
+}
+
+export default App;
